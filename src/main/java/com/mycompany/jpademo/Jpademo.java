a51a8d2cd04839b9dcfe112c2c2fc11e10fa9819
DanielBuilesG <daniel.builes99@gmail.com>
2023-07-18 09:47:31 -0500

    first commit

---
+++ src/main/java/com/mycompany/jpademo/Jpademo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jpademo;
+
+/**
+ *
+ * @author daniel.builes
+ */
+public class Jpademo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
